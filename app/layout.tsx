6d183c3140377585be12c4256b3c7a7f74e6a1d1
masummahmud5000 <mdmasumpresent@gmail.com>
2026-02-26 04:57:53 +0600

    Align navigation items in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
       <body>
         <nav>
           <div className="border-b-7 border-amber-500 bg-green-500 py-1 px-2"><Image src={logo} alt="School Logo" width={300}/></div>
-          <div className="flex text-center py-1 gap-7 bg-red-700 text-[13px] text-white ">
+          <div className="flex items-center justify-center py-1 gap-7 bg-red-700 text-[13px] text-white ">
             <Link className="hover:font-bold" href="#">হোম</Link>
             <Link className="hover:font-bold" href="#">স্কুল-সম্পর্কে</Link>
             <Link className="hover:font-bold" href="#">যেসব-সুবিধা-আছে</Link>
