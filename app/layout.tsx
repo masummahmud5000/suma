1bbcb16410554a8f33b0c6c1f0b07c76115dc046
mdmasum <mdmasumpresent@gmail.com>
2026-02-26 14:11:27 +0600

    home full

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import logo from '@/public/school-logo.png'
 import Image from "next/image";
 import Link from "next/link";
+import Marquee from "react-fast-marquee"
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 export default function RootLayout({
   children,
@@ -12,16 +14,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav>
-          <div className="border-b-7 border-amber-500 bg-green-500 py-1 px-2"><Image src={logo} alt="School Logo" width={300}/></div>
-          <div className="flex items-center justify-center py-1 gap-7 bg-red-700 text-[13px] text-white ">
+        <nav className="fixed top-0 left-0 w-full z-50">
+          <div className="border-b-7 border-amber-500 bg-green-500 py-1 px-2 lg:px-15 lg:py-2"><Image src={logo} alt="School Logo" width={300}/></div>
+          <div className="flex items-center justify-center py-1 gap-7 lg:gap-50 bg-red-700 text-[13px] text-white">
             <Link className="hover:font-bold" href="#">হোম</Link>
             <Link className="hover:font-bold" href="#">স্কুল-সম্পর্কে</Link>
             <Link className="hover:font-bold" href="#">যেসব-সুবিধা-আছে</Link>
-            <Link className="hover:font-bold" href="#">যুগাযুগ</Link>
+            <Link className="hover:font-bold" href="#">যোগাযোগ</Link>
+          </div>
+          <div className="flex items-center bg-blue-500">
+            <h1 className="fa fa-microphone text-white py-2 pl-2 pr-5 rounded-r-2xl bg-fuchsia-500"></h1>
+            <Marquee className="text-white font-bold" speed={50} direction="left">সন্তান আপনার দায়িত্ব আমাদের | আপনার সন্তান কে আমাদের হাতে তুলে পিটিয়ে লাল করে দিব! | সুলতান উদ্দীন মেমোরিয়াল একাডেমি .............Repeat............. </Marquee>
           </div>
         </nav>
         {children}
+        <footer className="flex flex-col gap-2 bg-black text-white py-7 rounded-t-2xl">
+          <div>
+            <h1 className="text-center">প্রতিষ্ঠাতা : <span className="text-cyan-300 font-bold">সালাহ উদ্দীন আহমেদ মিলন</span></h1>
+          </div>
+          <div>
+            <h1 className="text-center">© sultan-uddin-memorial-academy-2026</h1>
+          </div>
+          <div className="flex justify-center items-center gap-5">
+            <h1 className="fa fa-code"></h1>
+            <h1>Developer : <span className='font-bold text-indigo-300'> Masum Software Lab</span></h1>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import '@/app/globals.css'
 import homeImage from "@/public/home-image.jpg"
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 
   return(
-    <main className="px-3 py-7 h-screen bg-amber-100">
+    <main className="px-3 mt-35 py-7 h-screen bg-amber-100">
       <div className="flex flex-col gap-3">
         <h1 className="text-3xl pl-2 text-blue-500 font-bold">Welcome To, </h1>
         <h1 className="sName text-lg text-center font-bold bg-linear-to-b from-blue-700 to-emerald-600 py-1 px-2 rounded-xl text-white">Sultan Uddin Memorial Academy</h1>
       </div>
       <Image src={homeImage} alt="Home-Image" className="homeImage mt-6 w-full h-auto border-5 border-amber-600 rounded-3xl"/>
+      <div className="flex flex-col items-center mt-7 bg- py-8 gap-5 rounded-2xl">
+        <h1 className='text-lg text-fuchsia-500'>আজই আপনার সন্তানকে ভর্তি করে দিন</h1>
+        <h1 className='text-3xl'>আমাদের স্কুল এ</h1>
+        <Link href="#" className='animate-bounce border-3 border-amber-300 text-white bg-green-700 py-2 px-5 mt-10 rounded-xl'>ভর্তির জন্য যোগাযোগ করুন</Link>
+      </div>
     </main>
   );
 }
