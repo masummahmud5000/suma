23e8802097a29db051b7deeca9212294d4d5dce0
masummahmud5000 <mdmasumpresent@gmail.com>
2026-02-28 16:00:35 +0600

    Update marquee text in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           </div>
           <div className="flex items-center bg-blue-500">
             <h1 className="fa fa-microphone text-white py-2 pl-2 pr-5 rounded-r-2xl bg-fuchsia-500"></h1>
-            <Marquee className="text-white font-bold" speed={50} direction="left">সন্তান আপনার দায়িত্ব আমাদের | আপনার সন্তান কে আমাদের হাতে তুলে দিন, পিটিয়ে লাল করে দিব! | সুলতান উদ্দীন মেমোরিয়াল একাডেমি .............Repeat............. </Marquee>
+            <Marquee className="text-white font-bold" speed={50} direction="left">সন্তান আপনার দায়িত্ব আমাদের | আপনার সন্তান কে আমাদের হাতে তুলে দিন, মানুষ বানানোর দায়িত্ব আমাদের .............Repeat............. </Marquee>
           </div>
         </nav>
         {children}
